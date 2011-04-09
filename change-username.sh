24716f65113f142f31c45aaab5a98f8cd85c3926
fsmithred <fsmithred@gmail.com>
2011-04-08 21:10:46 -0400

    This replaces the version on 6.0.4-beta CD

--- change-username.sh
+++ change-username.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# change-username.sh  (Refracta xfce/icewm version)
+# change-username.sh  (Refracta xfce/icewm version 2)
 # Change user name from user to something else, for a live-build system
 # that was installed with refractainstaller.
 # Run once as user from $HOME with no arguments
@@ -22,182 +22,10 @@ check_exit () {
 [[ $? -eq 0 ]] || { echo "Exit due to error:  $?" ; exit 1 ; }
 }
 
-:<<HOLD
-replace_inittab () {
-echo "# /etc/inittab: init(8) configuration.
-# inittabreplacement
-# $Id: inittab,v 1.91 2002/01/25 13:35:21 miquels Exp $
 
-# The default runlevel.
-id:2:initdefault:
 
-# Boot-time system configuration/initialization script.
-# This is run first except when booting in emergency (-b) mode.
-si::sysinit:/etc/init.d/rcS
-
-# What to do in single-user mode.
-~~:S:wait:/sbin/sulogin
-
-# /etc/init.d executes the S and K scripts upon change
-# of runlevel.
-#
-# Runlevel 0 is halt.
-# Runlevel 1 is single-user.
-# Runlevels 2-5 are multi-user.
-# Runlevel 6 is reboot.
-
-l0:0:wait:/etc/init.d/rc 0
-l1:1:wait:/etc/init.d/rc 1
-l2:2:wait:/etc/init.d/rc 2
-l3:3:wait:/etc/init.d/rc 3
-l4:4:wait:/etc/init.d/rc 4
-l5:5:wait:/etc/init.d/rc 5
-l6:6:wait:/etc/init.d/rc 6
-# Normally not reached, but fallthrough in case of emergency.
-z6:6:respawn:/sbin/sulogin
-
-# What to do when CTRL-ALT-DEL is pressed.
-ca:12345:ctrlaltdel:/sbin/shutdown -t1 -a -r now
-
-# Action on special keypress (ALT-UpArrow).
-#kb::kbrequest:/bin/echo \"Keyboard Request--edit /etc/inittab to let this work.\"
-
-# What to do when the power fails/returns.
-pf::powerwait:/etc/init.d/powerfail start
-pn::powerfailnow:/etc/init.d/powerfail now
-po::powerokwait:/etc/init.d/powerfail stop
-
-# /sbin/getty invocations for the runlevels.
-#
-# The \"id\" field MUST be the same as the last
-# characters of the device (after \"tty\").
-#
-# Format:
-#  <id>:<runlevels>:<action>:<process>
-#
-# Note that on most Debian systems tty7 is used by the X Window System,
-# so if you want to add more getty's go ahead but skip tty7 if you run X.
-#
-#1:2345:respawn:/bin/login -f user </dev/tty1 >/dev/tty1 2>&1
-#2:23:respawn:/bin/login -f user </dev/tty2 >/dev/tty2 2>&1
-#3:23:respawn:/bin/login -f user </dev/tty3 >/dev/tty3 2>&1
-#4:23:respawn:/bin/login -f user </dev/tty4 >/dev/tty4 2>&1
-#5:23:respawn:/bin/login -f user </dev/tty5 >/dev/tty5 2>&1
-#6:23:respawn:/bin/login -f user </dev/tty6 >/dev/tty6 2>&1
-#
-1:2345:respawn:/sbin/getty 38400 tty1
-2:23:respawn:/sbin/getty 38400 tty2
-3:23:respawn:/sbin/getty 38400 tty3
-4:23:respawn:/sbin/getty 38400 tty4
-5:23:respawn:/sbin/getty 38400 tty5
-6:23:respawn:/sbin/getty 38400 tty6
-
-# Example how to put a getty on a serial line (for a terminal)
-#
-#T0:23:respawn:/bin/login -f user </dev/tty >/dev/tty 2>&1
-#T1:23:respawn:/bin/login -f user </dev/tty >/dev/tty 2>&1
-
-# Example how to put a getty on a modem line.
-#
-#T3:23:respawn:/bin/login -f user </dev/tty >/dev/tty 2>&1
-
-" > ./inittab.new
-}
-HOLD
-
-prepare_unlive () {          
-sudo passwd
-check_exit
-
-# copy the script to /root if it's not already there
-#if ! [[ -f /root/${0##*/} ]] ; then   # this does not work in refracta; user doesn't have permission
-if ! $(sudo ls /root | grep -q ${0##*/}) ; then
-    echo "    /root/${0##*/} does not exist.
-    Copying..."
-    sleep 4
-    sudo cp "$0" /root
-    sudo chmod +x /root/${0##*/}
-    check_exit
-    sleep 6
-fi
-
-:<<HOLD
-# make a backup copy of /etc/inittab if one doesn't exist
-if ! [[ -f /root/inittab.old ]]; then
-    echo "    /root/inittab.old does not exist.
-    Backing up /etc/inittab"
-    sleep 2
-    sudo cp /etc/inittab /root/inittab.old
-    check_exit
-else
-    echo "
-    inittab was already backed up as /root/inittab.old
-    "
-    sleep 2
-fi
-
-# replace inittab if it wasn't already done
-if $(grep -q inittabreplacement /etc/inittab) ; then
-    echo "
-    /etc/inittab was replaced previously. Leaving it alone.
-    "
-    sleep 2
-else
-    echo "    Replacing /etc/inittab to disable auto-login on tty.
-    "
-    sleep 2
-    replace_inittab
-    sudo cp ./inittab.new /etc/inittab
-    rm ./inittab.new
-    check_exit
-fi
-HOLD
-
-# drop to runlevel 1 to log out user from tty1-6
-echo "
-    Give root password when asked, then run this script from /root.
-    "
-sleep 4
-sudo init 1
-exit 0
-}
-
-# First run
-# If not root, then do the following stuff:
-if ! [[ $(id -un) = "root" ]] ; then
-    while true; do
-    echo "
-   You're not root. This must be the first run of mkunlive. It will 
-   let you create a root password, copy itself to /root, and then drop
-   to single-user mode (runlevel 1), where you can login as root 
-   and run the script again. If you need to get out of this script
-   before it kills your running desktop (for instance, to save some
-   open files) answer \"no\" now, and run the script when you're ready.
-    
-   Continue?    (y/n)
-    "
-    read ans
-        case $ans in
-          [Yy]*) prepare_unlive ;;
-          [Nn]*) exit 0 ;;
-        esac
-    done
-fi
-
-# Second run
-# If root, then do the following stuff:
-if [[ $(id -un) = "root" ]] ; then
-    echo "
-    You are root. This must be the second run of this script.
-    Create new user, edit /etc/sudoers, return to runlevel 2"
-    sleep 3
-fi
-
-# check runlevel
-if ! $(runlevel | grep -q 1) ; then
-    echo "    You need to drop to runlevel 1, then re-run this script."
-    exit 1
-fi
+# check root
+[[ $(id -un) = "root" ]] || { echo "You need to be root!" ; exit 1 ; }
 
 # check that there are two arguments
 if [[ $# -ne 2 ]] ; then
@@ -211,6 +39,13 @@ if ! $(grep -q $oldname /etc/passwd) ; then
     exit 1
 fi
 
+# make sure that the user isn't logged in
+if $(who | grep -q $oldname); then
+    echo "
+    $oldname is logged in and must log out before you can proceed.
+    "
+    exit 1
+fi
 echo "    Changing user name and group...
     "
 sleep 2
@@ -255,17 +90,6 @@ while true; do
     esac
 done
 
-
-# This might need to be added. I only had to do it when I changed
-# the user name manually.
-#echo "
-#  Adding $newname to groups...
-#  "
-#sleep 1
-#usermod -a -G dialout,cdrom,floppy,audio,video,plugdev,fuse $newname
-#echo $(id $newname)
-#echo
-#sleep 3
 
 echo "
     This script will attempt to replace every instance of 
@@ -322,25 +146,28 @@ done
 
 
 # Disable sudo-mode for gksu
-if [[ $edit_sudoers = "yes" ]]; then
-    zenity --question --title="Disable sudo mode for gksu" --ok-label="Yes, I commented out the line." --cancel-label="No, I replaced \"user\" with the new username." \
-     --text="If you commented out the last line in /etc/sudoers in the last step, one more file will be changed for you so that gksu will work properly."
-        if [[ $? = 0 ]]; then
-            if [[ -n $newname ]]; then
-                sed -i~ '/sudo-mode/s/true/false/' /target/home/"$newname"/.gconf/apps/gksu/%gconf.xml
-            else
-                sed -i~ '/sudo-mode/s/true/false/' /target/home/user/.gconf/apps/gksu/%gconf.xml
-            fi
-        fi
-fi
-
+while true; do
+echo "
+    If you commented out the last line in /etc/sudoers in the last 
+    step, answer \"yes\". However, if you changed the user name, then
+    answer \"no\".
+    
+    Another way to ask this question - Do you want to disable sudo mode
+    for gksu? If you answer \"yes\', then gksu will use root password.
+    "
+    read ans
+    case $ans in
+      [Yy]*) sed -i~ '/sudo-mode/s/true/false/' /home/"$newname"/.gconf/apps/gksu/%gconf.xml ; break ;;
+      [Nn]*) break ;;
+    esac
+done
 
 echo "
     Done!
     
-    Returning to runlevel 2    
+    Restarting the xserver.   
     "
 sleep 2
+/etc/init.d/gdm restart
 
-init 2
 exit 0
